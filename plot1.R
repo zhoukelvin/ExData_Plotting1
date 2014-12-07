c3d1c3d5823309c2456628f825c9e47eab611e26
zhoukelvin <zhoujinwen225@126.com>
2014-12-08 03:45:35 +0800

    Revert "fff"
    This reverts commit f139b3279e849dfdf1d1528cadd30f8750bb916f.

--- plot1.R
+++
@@ -1,7 +0,0 @@
-
-Sys.setlocale("LC_ALL", "English")
-data<- read.csv("household_power_consumption.txt",na.strings="?",fill=T,sep=";",header=T)
-data<- data[as.Date(data$Date) == as.Date("01/02/2007")|as.Date(data$Date) == as.Date("02/02/2007"),]
-png("plot1.png",width=480,height=480,units='px')
-hist(data$Global_active_power,col="red",xlab="Global Active Power(kilowatts) ",main="Global Active Power")
-dev.off()
